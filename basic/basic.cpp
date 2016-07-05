cd12d725e1dc7cc7253824501e60d2064ecb8706
muzie <muzie@neighbor21.co.kr>
2016-07-05 15:57:59 +0900

    basic. 영상씨의 문제.. 프로토타입 선언은 함수 내에서도 가능..

---
+++ basic/basic.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class SomeClass
+{
+public:
+    SomeClass()
+    {
+        std::cout << "this is ctr" << std::endl;
+    }
+
+    SomeClass s1()
+    {
+        std::cout << "it's function" << std::endl;
+        return *this;
+    }
+
+    int operator()()
+    {
+        std::cout << "this is operator overloading" << std::endl;
+        return 0;
+    }
+
+    virtual ~SomeClass() {}
+};
+
+int main(void)
+{
+    std::cout << "----------" << std::endl;
+    SomeClass s;
+    std::cout << "----------" << std::endl;
+    SomeClass s1();
+    std::cout << "----------" << std::endl;
+
+    return 0;
+}
